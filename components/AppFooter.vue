0ff576e6e10844da4566298c89aec23ee483f7bf
RomZor <romain.richard.19@gmail.com>
2023-02-19 18:55:01 +0100

    Initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="w-full bg-red-500">
+    le footer
+  </footer>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <header class="flex justify-between p-4 w-full bg-red-500">
+    <span>Pokedex</span>
+    <button>Rechercher</button>
+  </header>
+</template>
