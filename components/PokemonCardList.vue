5bf8fff8b832ff57f1d2e7ed6766e8b4171eab29
RomZor <romain.richard.19@gmail.com>
2023-02-21 17:41:40 +0100

    WIP card pokemon list

--- components/PokemonCardList.vue
+++ components/PokemonCardList.vue
@@ -3,5 +3,12 @@
     <div>
       <img src="https://www.pokepedia.fr/images/3/3f/Sprite_0001_HOME.png" alt="Bulbizarre">
     </div>
+    <div class="flex flex-col items-center">
+      <h4 class="font-semibold">Bulbizarre</h4>
+      <div class="space-x-2">
+        <span class="rounded-full bg-green-500 text-white px-2">Plante</span>
+        <span class="rounded-full bg-purple-500 text-white px-2">Poison</span>
+      </div>
+    </div>
   </div>
 </template>
