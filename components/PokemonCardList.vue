970767137f3ae15024eab4ff4ccfd58579b09487
RomZor <romain.richard.19@gmail.com>
2023-02-21 17:39:04 +0100

    WIP Pokemon list

---
+++ components/PokemonCardList.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="border rounded">
+    <div>
+      <img src="https://www.pokepedia.fr/images/3/3f/Sprite_0001_HOME.png" alt="Bulbizarre">
+    </div>
+  </div>
+</template>
---
+++ components/PokemonList.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="grid md:grid-cols-2 lg:grid-cols-4 md:gap-4">
+    <PokemonCardList />
+  </section>
+</template>
