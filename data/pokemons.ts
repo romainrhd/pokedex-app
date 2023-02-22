42c268e0b56bbec75517206c25b16439f50a6f85
Romain <romain.richard.19@gmail.com>
2023-02-22 22:28:35 +0100

    Add pokemons data with appearances

---
+++ data/pokemons.ts
@@ -0,0 +1,93 @@
+export const pokemons = [
+  {
+    "nationalNumber": 1,
+    "name": "Bulbizarre",
+    "evolutionOfNationalNumber": null,
+    "appearances": [
+      {
+        "name": null,
+        "picture": "https://www.pokepedia.fr/images/3/34/Sprite_001_HOME.png",
+        "isDefault": true,
+        "isShiny": false,
+        "types": ['Plante', 'Poison']
+      },
+      {
+        "name": null,
+        "picture": "https://www.pokepedia.fr/images/b/bf/Sprite_001_chromatique_HOME.png",
+        "isDefault": false,
+        "isShiny": true,
+        "types": ['Plante', 'Poison']
+      }
+    ],
+  },
+  {
+    "nationalNumber": 2,
+    "name": "Herbizarre",
+    "evolutionOfNationalNumber": 1,
+    "appearances": [
+      {
+        "name": null,
+        "picture": "https://www.pokepedia.fr/images/5/52/Sprite_0002_HOME.png",
+        "isDefault": true,
+        "isShiny": false,
+        "types": ['Plante', 'Poison']
+      },
+      {
+        "name": null,
+        "picture": "https://www.pokepedia.fr/images/f/f5/Sprite_0002_chromatique_HOME.png",
+        "isDefault": false,
+        "isShiny": true,
+        "types": ['Plante', 'Poison']
+      }
+    ],
+  },
+  {
+    "nationalNumber": 3,
+    "name": "Florizarre",
+    "evolutionOfNationalNumber": 2,
+    "appearances": [
+      {
+        "name": null,
+        "picture": "https://www.pokepedia.fr/images/b/b3/Sprite_0003_%E2%99%82_HOME.png",
+        "isDefault": true,
+        "isShiny": false,
+        "types": ['Plante', 'Poison']
+      },
+      {
+        "name": null,
+        "picture": "https://www.pokepedia.fr/images/3/3d/Sprite_0003_%E2%99%82_chromatique_HOME.png",
+        "isDefault": false,
+        "isShiny": true,
+        "types": ['Plante', 'Poison']
+      },
+      {
+        "name": "Méga-Florizarre",
+        "picture": "https://www.pokepedia.fr/images/9/9c/Sprite_0003_M%C3%A9ga_HOME.png",
+        "isDefault": true,
+        "isShiny": false,
+        "types": ['Plante', 'Poison']
+      },
+      {
+        "name": "Méga-Florizarre",
+        "picture": "https://www.pokepedia.fr/images/9/93/Sprite_0003_M%C3%A9ga_chromatique_HOME.png",
+        "isDefault": false,
+        "isShiny": true,
+        "types": ['Plante', 'Poison']
+      },
+      {
+        "name": "Florizarre Gigamax",
+        "picture": "https://www.pokepedia.fr/images/6/6c/Sprite_0003_Gigamax_HOME.png",
+        "isDefault": true,
+        "isShiny": false,
+        "types": ['Plante', 'Poison']
+      },
+      {
+        "name": "Florizarre Gigamax",
+        "picture": "https://www.pokepedia.fr/images/c/c4/Sprite_0003_Gigamax_chromatique_HOME.png",
+        "isDefault": false,
+        "isShiny": true,
+        "types": ['Plante', 'Poison']
+      },
+    ],
+  }
+]
